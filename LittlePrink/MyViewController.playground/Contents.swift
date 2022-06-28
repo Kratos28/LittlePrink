2172c952b7be691ed186a0195c5c3a9f7acfcfbe
Kratos28 <c193293@126.com>
2022-06-28 19:31:22 +0800

    ffffqw

--- LittlePrink/MyViewController.playground/Contents.swift
+++ LittlePrink/MyViewController.playground/Contents.swift
@@ -3,16 +3,23 @@
 import UIKit
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
+class MyViewController : UIViewController ,UICollectionViewDelegate,UICollectionViewDataSource {
     
-    let
-   private lazy var cv: UICollectionView {
+    
+    let photos = [
+       UIImage(named: "1"),
+       UIImage(named: "2"),
+       UIImage(named: "3")
+
+   ];
+    
+        lazy var cv: UICollectionView {
        let layout = UICollectionViewFlowLayout();
        layout.scrollDirection = .horizontal;
        layout.itemSize = CGSize(width: 90, height: 90);
-       cv.translatesAutoresizingMaskIntoConstraints = false;
        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout:layout);
-       
+       cv.translatesAutoresizingMaskIntoConstraints = false;
+
        cv.register(CVCell.self, forCellWithReuseIdentifier: "CVCellID");
        return cv;
     }
@@ -32,11 +39,45 @@ class MyViewController : UIViewController {
         cv.leftAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true;
         cv.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 16).isActive = true;
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CVCellID", for: indexPath) as! CVCell
+        cell.imageview.image = self.photos[indexPath.item];
+        return cell;
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count;
+    }
+    
 }
+
+
+
 
 class CVCell :UICollectionViewCell
 {
+    lazy var imageview:UIImageView = {
+        let imagview = UIImageView();
+        imagview.translatesAutoresizingMaskIntoConstraints = false;
+        imagview.contentMode = .scaleAspectFill;
+        imagview.backgroundColor = .systemBackground;
+        return imagview;
+    }();
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+        addSubview(imageview);
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func setUI(){
+        imageview.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true;
+        imageview.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true;
+        imageview.topAnchor.constraint(equalTo: topAnchor, constant: 0).isActive = true;
+        imageview.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true;
+
+    }
 }
 
 PlaygroundPage.current.liveView = MyViewController()
