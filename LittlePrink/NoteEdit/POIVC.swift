b19ed92b49b12bde47a898fd1f5dd2999a3926cf
Kratos28 <c193293@126.com>
2022-07-21 23:07:16 +0800

    ff

---
+++ LittlePrink/NoteEdit/POIVC.swift
@@ -0,0 +1,39 @@
+//
+//  POIVC.swift
+//  LittlePrink
+//
+//  Created by Kratos on 2022/7/20.
+//
+
+import UIKit
+
+class POIVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension POIVC:UITableViewDataSource
+{
+    
+}
+
+
+extension POIVC:UITableViewController
+{
+    
+}
