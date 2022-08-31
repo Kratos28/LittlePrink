51624727c7b86bd2499a24c1b085c4ef6c2dcbad
Kratos28 <c193293@126.com>
2022-09-01 00:48:58 +0800

    123

--- LittlePrink/Login/SocialLoginVC.swift
+++ LittlePrink/Login/SocialLoginVC.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Alamofire
 class SocialLoginVC: UIViewController {
 
     override func viewDidLoad() {
@@ -21,7 +21,37 @@ class SocialLoginVC: UIViewController {
         guard let signer =  APRSASigner(privateKey: "支付宝创建的私钥"),let signedStr   = signer.sign(infoStr, withRSA2: true) else {return;}
         let authInfoStr = "\(infoStr)&sign=\(signedStr)";
         AlipaySDK.defaultService().auth_V2(withInfo: authInfoStr, fromScheme: kAppScheme) { result in
-            print(result);
+            guard let res = result else{return}
+            let resStatus = res["resultStatus"] as! String
+            if resStatus == "9000"{
+                let resStr = res["result"] as! String
+                
+                let resArr = resStr.components(separatedBy: "&");
+                for subRes in resArr{
+                    let equalIndex = subRes.firstIndex(of: "=")!;
+                    let equalEndIndex =  subRes.index(after: equalIndex);
+                    let prefix = subRes[..<equalIndex];
+                    let suffix = subRes[equalEndIndex...];
+                    print(prefix);
+                    if subRes.hasPrefix("auth_code")
+                    {
+                        print("authCode = \(suffix)");
+                        let parameters = [
+                            "timestamp":Date().format(with: "yyyy-MM-dd HH:mm:ss"),
+                            "method":"alipay.system.oauth.token",
+                            "app_id":kAliPayPID,
+                            "sign_type":"RSA2",
+                            "version":"1.0",
+                            "charset":"utf-8",
+                            "grant_type":"authorization_code",
+                            "code":String(suffix)
+                        ];
+                        AF.request("https://openapi.alipay.com/gateway.do", parameters: parameters)
+                        
+                    }
+                }
+                
+            }
         };
         
         
